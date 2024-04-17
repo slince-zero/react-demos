0bfc8348ff274540d6b8747e59ea6ed37d992b54
slince-zero <1711257704@qq.com>
2024-04-17 12:25:31 +0800

    修复taildwindcss 没有生效问题，按着官方文档走vite路线一步一步来

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -1,3 +1,13 @@
 export default function Navbar() {
-    return <div>navbar</div>
+  return (
+    <nav className="flex justify-between items-center py-8 container flex-col lg:flex-row gap-5 lg:gap-0">
+      <h2 className="text-2xl font-semibold">FoodRecipe</h2>
+      <input
+        className="bg-white bg-opacity-75 p-3 px-8 rounded-full outline-none lg:w-96 shadow-lg shadow-red-100 focus:shadow-red-200"
+        type="text"
+        name="search"
+        placeholder="输入内容"
+      />
+    </nav>
+  )
 }
